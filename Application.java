75e80c5983e417b1a95e2ddd5d66b60759c207f6
Mbannicova <87451655+Mbannicova@users.noreply.github.com>
2021-08-24 02:26:36 +0300

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,19 @@
+package main;
+
+import geometry.Line;
+
+public class Application {
+	
+
+	public static void main(String[] args) {
+		Line short_horizontal_line = new Line( 100,0, 200,0 );
+	      short_horizontal_line.printCoords(100,0, 200,0);
+	      System.out.println( short_horizontal_line.length(100,0, 200,0) );
+	      
+	      Line long_diagonal_line = new Line( 100,100, 500,500 );
+	      long_diagonal_line.printCoords(100,100, 500,500);
+	      System.out.println( long_diagonal_line.length(100,100, 500,500) );
+
+	}
+
+}
